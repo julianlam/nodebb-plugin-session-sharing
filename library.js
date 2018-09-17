3721a5a90d22f9da70d763a76645bb65f7f4eaff
Julian Lam <julian@nodebb.org>
2018-09-17 16:11:20 -0400

    setting req.loggedIn on session-sharing login

--- library.js
+++ library.js
@@ -436,6 +436,7 @@ plugin.addMiddleware = function(req, res, next) {
 		
 						winston.verbose('[session-sharing] Processing login for uid ' + uid + ', path ' + req.originalUrl);
 						req.uid = uid;
+						req.loggedIn = true;
 						nbbAuthController.doLogin(req, uid, function () {
 							req.session.loginLock = true;
 							res.redirect(req.originalUrl);
