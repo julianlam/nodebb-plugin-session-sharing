julianlam/nodebb-plugin-session-sharing

commit bc17bf70fa77e60376933bfd11448aebdb9c6e55

fix: #138, build settings object against a frozen set of defaults

library.js
@@ -54,6 +54,8 @@ const plugin = {
 	},
 };
 
+plugin.defaults = Object.freeze({ ...plugin.settings });
+
 payloadKeys.forEach(function (key) {
 	plugin.settings['payload:' + key] = key;
 });
@@ -566,7 +568,7 @@ plugin.reloadSettings = async (data) => {
 	}
 
 	winston.info('[session-sharing] Settings OK');
-	plugin.settings = _.defaults(_.pickBy(settings, Boolean), plugin.settings);
+	plugin.settings = _.defaults(_.pickBy(settings, Boolean), plugin.defaults);
 	plugin.ready = true;
 };
 

commit ce912e49d75041d1773d0fc7ef03f61c284575fa

fix: regression caused by bc17bf70fa77e60376933bfd11448aebdb9c6e55

library.js
@@ -54,11 +54,11 @@ const plugin = {
 	},
 };
 
-plugin.defaults = Object.freeze({ ...plugin.settings });
-
 payloadKeys.forEach(function (key) {
 	plugin.settings['payload:' + key] = key;
 });
+
+plugin.defaults = Object.freeze({ ...plugin.settings });
 
 plugin.init = async (params) => {
 	var router = params.router;